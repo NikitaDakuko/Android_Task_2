7b8078f989e9035d90690c448382aeb386b99b83
Дакуко Никита <nikita.dakuko@gmail.com>
2018-12-21 20:47:46 +0300

    Initial commit

---
+++ app/src/main/java/nikitadakuko/lab/lab2/MainActivity.java
@@ -0,0 +1,31 @@
+package nikitadakuko.lab.lab2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.editText);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(),SecondActivity.class);
+                in.putExtra("data", editText.getText().toString());
+
+                startActivity(in);
+            }
+        });
+
+    }
+}
